ae36053a87688ccd0b975ba404768281b1547317
Freja <frejaeb@gmail.com>
2023-08-24 14:44:44 +0200

    js produukt mfejl

---
+++ produkt.js
@@ -0,0 +1,17 @@
+// https://kea-alt-del.dk/t7/api/products/1525 //
+
+fetch("https://kea-alt-del.dk/t7/api/products/1525");
+.then((response) => response.json())
+.then(data => showProdukt(data));
+
+function showProdukt(produkt){
+    console.log(produkt);
+    document.querySelector(".produkt_1 h3").textContent =
+     produkt.productdisplayname;
+
+    document.querySelector(".produkt_1 .brand").textContent =
+     produkt.brandname;
+
+    document.querySelector("img").src = `https://kea-alt-del.dk/t7/images/webp/640/${product.id}.webp`
+
+}
