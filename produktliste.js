294536bd5ba4bbbe58c9d3b71e62416e87351780
Freja <frejaeb@gmail.com>
2023-08-28 13:46:34 +0200

    produktli js

--- produktliste.js
+++ produktliste.js
@@ -8,15 +8,31 @@ function showProducts(products) {
 }
 
 function showProduct(produkt) {
-  console.log(produkt);
+  //console.log(produkt);
   //fange templates
   const template = document.querySelector("#produkyLegginsTemplate").content;
   //lave kopi
   const copy = template.cloneNode(true);
   //ændre indhold
-
+  copy.querySelector("h3").textContent = produkt.productdisplayname;
+  if (produkt.soldout) {
+    //produkt er udsolt
+    copy.querySelector("article").classList.add("soldOut");
+  }
   //appende
+  document.querySelector("main").appendChild(copy);
 }
+
+// indhold fr html
+
+/*
+  <article>
+
+                    <img src="https://kea-alt-del.dk/t7/images/webp/640/1573.webp" alt="">
+                    <h3>Leggins 300kr</h3>
+                    <a href="produkt.html">Læs mere</a>
+                </article>
+/*
 
 /* {
   "id": 1528,
